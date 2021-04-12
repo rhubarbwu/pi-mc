44b542af0513b6f724bbdb9bf288fabee9d3d71f
Rupert Wu <wu.rupert@outlook.com>
2021-04-11 23:48:35 -0400

    Vectorized with numpy arrays.

--- pi-python/script.py
+++ pi-python/script.py
@@ -1,46 +1,47 @@
-from math import sqrt
-from random import uniform
+import numpy as np
 
 # radius of circle, half side length of square
 r = 1000.
 
 
-def gen(x):
-    num = uniform(0.0, x)
-    return num
+def gen(n_trials):
+    return np.random.uniform(0.0, r * 2, n_trials)
 
 
 def in_circle(x, y):
     d_x, d_y = r - x, r - y
     d_2 = d_x * d_x + d_y * d_y
-    d = sqrt(d_2)
+    d = np.sqrt(d_2)
 
-    return d <= r
+    return (d <= r)
 
 
 def in_bottom_left(x, y):
-    return x <= r and y <= r
+    return (x <= r) * (y <= r)
 
 
-def run_trial(n):
-    n_circle = 0
-    n_bottom_left = 0
+def run_trials(n_trials, n_points):
+    n_circle = np.zeros(n_trials, dtype=int)
+    n_bottom_left = np.zeros(n_trials, dtype=int)
 
-    for _ in range(n):
-        x, y = gen(r * 2), gen(r * 2)
-        n_circle += 1 if in_circle(x, y) else 0
-        n_bottom_left += 1 if in_bottom_left(x, y) else 0
+    for _ in range(n_points):
+        x, y = gen(n_trials), gen(n_trials)
+        n_circle += in_circle(x, y)
+        n_bottom_left += in_bottom_left(x, y)
 
     return n_circle / n_bottom_left
 
 
-n = 1000000
-n_trials = 1000
-estimates = []
-for _ in range(n_trials):
-    estimate = run_trial(n)
-    print(estimate)
-    estimates.append(estimate)
+from sys import argv
 
-from numpy import mean, var
-print("\nMean: {}, Variance: {}".format(mean(estimates), var(estimates)))
+n_trials, n_points = 1000, 1000000
+if len(argv) >= 3:
+    n_trials, n_points = int(argv[1]), int(argv[2])
+
+estimates = run_trials(n_trials, n_points)
+for est in estimates:
+    print(est)
+
+print()
+print(f"Mean: {np.mean(estimates)}")
+print(f"Variance: {np.var(estimates)}")

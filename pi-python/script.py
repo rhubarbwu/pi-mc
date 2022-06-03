fbc0672d17fb66e753df960a2fa84e276aad2d2c
Rupert Wu <wu.rupert@outlook.com>
2022-06-03 14:12:13 -0400

    Vectorize sampling with numpy.array.

--- pi-python/script.py
+++ pi-python/script.py
@@ -4,8 +4,8 @@ import numpy as np
 r = 1000.
 
 
-def gen(x):
-    num = np.random.uniform(0.0, x)
+def gen(n_trials):
+    num = np.random.uniform(0.0, r * 2, n_trials)
     return num
 
 
@@ -14,27 +14,27 @@ def in_circle(x, y):
     d_2 = d_x * d_x + d_y * d_y
     d = np.sqrt(d_2)
 
-    return d <= r
+    return (d <= r)
 
 
 def in_bottom_left(x, y):
-    return x <= r and y <= r
+    return (x <= r) * (y <= r)
 
 
-def run_trial(n):
-    n_circle = 0
-    n_bottom_left = 0
+def run_trials(n_trials, n_points):
+    n_circle = np.zeros(n_trials, dtype=int)
+    n_bottom_left = np.zeros(n_trials, dtype=int)
 
-    for _ in range(n):
-        x, y = gen(r * 2), gen(r * 2)
-        n_circle += 1 if in_circle(x, y) else 0
-        n_bottom_left += 1 if in_bottom_left(x, y) else 0
+    for _ in range(n_points):
+        x, y = gen(n_trials), gen(n_trials)
+        n_circle += in_circle(x, y)
+        n_bottom_left += in_bottom_left(x, y)
 
     return n_circle / n_bottom_left
 
 
-n = 1000000
 n_trials = 1000
-estimates = np.array([run_trial(n) for _ in range(n_trials)])
+n_points = 1000000
+estimates = run_trials(n_trials, n_points)
 
 print("\nMean: {}, Variance: {}".format(np.mean(estimates), np.var(estimates)))
